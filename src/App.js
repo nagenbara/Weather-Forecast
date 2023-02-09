f1f5dc4e628e888df8e17c2115cc2c1508549c40
rafif gunawan <rafifgunawan88@gmail.com>
2023-02-09 15:35:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+
+require("dotenv").config();
 
 function App() {
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = `http://api.weatherstack.com/current?access_key=${API_KEY}&&timestamp=${Date.now()}&query=${query}`;
+
+  const fetchData = async () => {
+    const response = await fetch(API_URL);
+    const result = await response.json();
+    setWeather(result);
+    setLoading(false);
+  };
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setWeather({});
+    setLoading(true);
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {weather.current ? (
+        <div className="weather-container">
+          <h2>Weather in {weather.location.name}, {weather.location.country}</h2>
+          <p>Temperature: {weather.current.temperature}°C</p>
+          <p>Feels like: {weather.current.feelslike}°C</p>
+          <p>Weather conditions: {weather.current.weather_descriptions[0]}</p>
+        </div>
+      ) : ( !loading ? 
+        <p>Masukkan Nama Kota Yang Valid</p> : <p> Loading... </p>
+      )}
     </div>
   );
 }
